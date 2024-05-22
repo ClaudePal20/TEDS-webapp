8a4875d51ebdf6e4f6b85c49277154aec6433aaf
ClaudePal20 <claudio.palacio@uabc.edu.mx>
2024-05-22 12:40:31 -0700

    Webapp creada

---
+++ src/App.js
@@ -0,0 +1,220 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='main-container'>
+      <div className='root'>
+        <div className='background'>
+          <div className='image' />
+        </div>
+        <div className='groups'>
+          <div className='image-1' />
+          <span className='transaction-details'>Transaction Details</span>
+          <span className='track'>Track</span>
+          <div className='image-2' />
+          <div className='image-3' />
+        </div>
+        <span className='view-transaction-details'>
+          View transaction details
+        </span>
+        <div className='groups-4'>
+          <div className='image-5' />
+        </div>
+        <div className='groups-6'>
+          <div className='background-7'>
+            <div className='flex-row-ce'>
+              <span className='transaction-type'> Transaction type</span>
+              <div className='image-8' />
+              <span className='tran'>Tran</span>
+            </div>
+            <span className='transaction-status'>Transaction status</span>
+            <span className='transaction'>Transaction</span>
+            <div className='image-9' />
+            <span className='total-transaction-amount'>
+              Total transaction amount
+            </span>
+            <span className='transaction-identification-code'>
+              Transaction identification code
+            </span>
+            <span className='transaction-initiator-details'>
+              Transaction initiator details
+              <br />
+              Transaction recipient
+            </span>
+          </div>
+          <div className='button'>
+            <button className='background-a'>
+              <span className='moreinfo'>Moreinfo</span>
+            </button>
+          </div>
+        </div>
+        <div className='groups-b'>
+          <div className='background-c'>
+            <span className='receive-transaction'>Receive transaction</span>
+            <span className='transaction-d'>Transaction</span>
+            <div className='image-e' />
+            <span className='total-amount-blockchain'>
+              Total amount in blockchain
+            </span>
+            <span className='transaction-identification-number'>
+              Transaction identification number
+            </span>
+            <span className='transaction-initiator'>Transaction initiator</span>
+            <span className='transaction-recipient'>Transaction recipient</span>
+            <button className='moreinfo-f' />
+            <span className='check-specific'>Check specific</span>
+            <span className='upda'>Upda</span>
+            <div className='image-10' />
+          </div>
+        </div>
+        <div className='groups-11'>
+          <div className='background-12'>
+            <div className='flex-row-d'>
+              <span className='transaction-completion'>
+                Transaction completion
+              </span>
+              <div className='image-13' />
+              <span className='tran-14'>Tran</span>
+            </div>
+            <span className='lorem-ipsum'>Lorem ipsum</span>
+            <span className='transaction-15'>Transaction</span>
+            <div className='image-16' />
+            <span className='transaction-breakdown-details'>
+              Transaction breakdown details
+            </span>
+            <span className='transaction-breakdown-summary'>
+              
+              Transaction breakdown summary
+            </span>
+            <span className='transaction-breakdown-initiator'>
+              Transaction breakdown initiator
+            </span>
+            <span className='transaction-breakdown'>Transaction breakdown</span>
+          </div>
+        </div>
+        <div className='groups-17'>
+          <div className='image-18' />
+        </div>
+        <div className='groups-19'>
+          <div className='image-1a' />
+        </div>
+        <div className='button-1b'>
+          <div className='background-1c'>
+            <button className='moreinfo-1d' />
+          </div>
+        </div>
+        <span className='transaction-card'>Transaction Cards</span>
+        <div className='groups-1e'>
+          <div className='groups-1f'>
+            <div className='background-20'>
+              <span className='transaction-21'>Transaction</span>
+              <span className='eth'>ETH10.25</span>
+              <span className='sender-address'>SenderAddress</span>
+              <div className='image-22'>
+                <div className='image-23' />
+              </div>
+              <div className='image-24' />
+              <span className='ox-ab-cd-ef'>OxAbCdEf1234</span>
+              <span className='recipie'>Recipie</span>
+            </div>
+          </div>
+        </div>
+        <div className='groups-25'>
+          <div className='groups-26'>
+            <div className='background-27'>
+              <div className='flex-row'>
+                <div className='image-28' />
+                <div className='image-29' />
+              </div>
+              <span className='view-available-t'>
+                View available transaction
+              </span>
+              <span className='transaction-2a'>Transaction</span>
+              <span className='masked'>Masked</span>
+              <div className='flex-row-b'>
+                <span className='transaction-2b'>Transaction</span>
+                <span className='block'>Block</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='background-2c'>
+          <div className='background-2d'>
+            <span className='transaction-type-2e'>Transaction Type</span>
+            <div className='image-2f' />
+            <span className='blockchain'>Blockchain</span>
+            <div className='flex-row-de'>
+              <span className='digital'>Digital</span>
+              <span className='verifi'>Verifi</span>
+              <div className='image-30' />
+            </div>
+            <span className='transaction-id'>Transaction ID</span>
+            <div className='flex-row-31'>
+              <span className='xabcdef'>0x1A2B3C4D5E6F</span>
+              <span className='q'>Q</span>
+            </div>
+            <span className='block-height'>Block Height</span>
+            <div className='flex-row-fcb'>
+              <span className='empty'>1234</span>
+              <span className='confirmed'>Confirmed</span>
+              <div className='image-32' />
+            </div>
+            <span className='hash'>Hash</span>
+            <div className='flex-row-dac'>
+              <div className='image-33' />
+              <span className='secure-connection'>Secure Connection</span>
+              <div className='image-34' />
+            </div>
+          </div>
+        </div>
+        <div className='background-35'>
+          <div className='background-36'>
+            <div className='background-37'>
+              <span className='btc'>BTC 5.75</span>
+              <div className='image-38' />
+              <span className='transaction-type-39'>Transaction Type</span>
+              <div className='flex-row-fa'>
+                <span className='digital-3a'>Digital</span>
+                <span className='verified'>Verified</span>
+                <div className='image-3b' />
+              </div>
+              <div className='groups-3c'>
+                <span className='transaction-id-3d'>Transaction ID</span>
+                <div className='flex-row-de-3e'>
+                  <span className='xabcdef-3f'>0x1A2B3C4D5E6F</span>
+                  <div className='image-40' />
+                </div>
+              </div>
+              <span className='block-height-41'>Block Height</span>
+              <div className='group-6'>
+                <span className='confirmed-42'>78901</span>
+                <span className='image-43'>Confirmed</span>
+                <div className='hash-44' />
+              </div>
+              <span className='flex-row-a'>Hash</span>
+              <div className='image-45'>
+                <div className='secure-connection-46' />
+                <span className='image-47'>Secure Connection</span>
+                <div className='groups-48' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='background-49'>
+          <div className='view-details'>
+            <span className='groups-4a'>View Details</span>
+          </div>
+        </div>
+        <div className='background-4b'>
+          <div className='view-details-4c'>
+            <span className='text-3f'>View Details</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;

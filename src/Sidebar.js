86e02c3c923ad263fe81bb7a030e7812fea5c256
ClaudePal20 <claudio.palacio@uabc.edu.mx>
2024-05-29 01:20:17 -0700

    UI mejorada

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,12 +5,12 @@ import './Sidebar.css'; // Importamos el archivo CSS para estilar
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h2>CimaTrade</h2>
-      <ul>
-        <li>Transacciones</li>
-        <li>Trading</li>
-        <li>Bolsa</li>
-      </ul>
+      <h2 id="titulo">CimaTrade</h2>
+      <div id="transacciones">
+        <h3 >Transacciones</h3>
+      </div>
+      <h3>Trading</h3>
+      <h3>Bolsa</h3>
     </div>
   );
 }
